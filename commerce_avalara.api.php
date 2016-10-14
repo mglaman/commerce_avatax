8fe19338c76bc9bda792c99cce077fe9b79104f9
Jonathan Sacksick <jonathan.sacksick@gmail.com>
2016-10-14 15:52:27 +0300

    Add a hook_commerce_avalara_create_transaction_alter().

---
+++ commerce_avalara.api.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * Allows modules to alter the create transaction request before its sent to the
+ * Avatax API.
+ *
+ * @param array $request_body
+ *   The request body array.
+ * @param object $order
+ *   The order object.
+ *
+ * @see commerce_avalara_create_transaction().
+ */
+function hook_commerce_avalara_create_transaction_alter(&$request_body, $order) {
+
+}
